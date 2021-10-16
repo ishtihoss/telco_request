782820c515951d79fe1977426f6f284ece4e70c8
ishtihoss <ishtiaque.hoss89@gmail.com>
2021-10-16 14:39:33 +0800

    added paths to necessary datasets

--- yoodoo.py
+++ yoodoo.py
@@ -1,3 +1,11 @@
 '''
 yoodoo request
 '''
+# ingest household id data
+
+hid_path = "s3a://ada-platform-components/Household/household/MY/restrict_ip/2020-06-01_2020-06-30/*"
+hid_df  = spark.read.parquet(hid_path)
+
+# ingest telco market insight data
+
+tmi_path = 's3a://ada-geogrid-feature-set/telco_market_insight'
