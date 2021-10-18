11732797e6a72ada4fdac23518b21b3b733ed9d1
ishtihoss <ishtiaque.hoss89@gmail.com>
2021-10-18 17:08:34 +0800

    lots of stuff

--- yoodoo.py
+++ yoodoo.py
@@ -7,10 +7,11 @@ from pyspark import SparkContext, SparkConf, HiveContext
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
 from math import radians, cos, sin, asin, sqrt
-from pyspark.sql.functions import *
 from pyspark.sql.window import Window
 from pyspark.sql.functions import lit
 from pyspark.sql.types import StringType
+
+
 
 
 # ingest household id data
@@ -35,4 +36,50 @@ jdfx = jdf.withColumn('carriers',F.explode('cellular_carriers')).drop('cellular_
 # groupby and collect list
 
 jdf1 = jdfx.groupby('component').agg(F.collect_list('carriers'))
-jdf1.take(5)
+jdf1 = jdf1.withColumnRenamed('collect_list(carriers)','carrier_list')
+
+
+jdf2 = jdf1.filter(F.size('carrier_list') > 0)
+
+# find out distinct list of carriers
+
+list = jdfx.select('carriers').distinct()
+
+carrier_list = list.select('carriers').rdd.flatMap(lambda x: x).collect()
+
+# Create python function for UDF
+
+def cel_per(carriers):
+    return(sum([1 for c in carriers if c=='Celcom']))
+
+
+# create UDF
+
+cel_perUDF = F.udf(lambda x: cel_per(x), T.IntegerType())
+
+# create new column with counts
+
+exp = jdf2.withColumn("count",cel_perUDF(F.col("carrier_list")))
+
+
+# Sample code for reference
+df.select(col("Seqno"), \
+    convertUDF(col("Name")).alias("Name") ) \
+   .show(truncate=False)
+
+
+# Some sample code
+
+from pyspark.sql import functions as F
+from pyspark.sql import types as T
+from pyspark.sql import Window
+
+subtag_music = ["a life in music", "music for life", "bso", "hans zimmer"]
+
+def subcadena_en_vector(tags):
+    return(sum([1 for c in tags if "music" in c]))
+
+print(subcadena_en_vector(subtag_music))
+
+subtag_music_UDF = F.udf(subcadena_en_vector, T.IntegerType())
+videosOcurrenciasMusicDF = videosDiasViralDF.withColumn("ocurrencias_music", subtag_music_UDF(F.col("tags")))
