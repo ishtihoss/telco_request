323a4376cf2146501481fca9c0175dc8b1fdc843
ishtihoss <ishtiaque.hoss89@gmail.com>
2021-10-18 12:26:00 +0800

    exploded nested array and collected in single column per ifa and household

--- yoodoo.py
+++ yoodoo.py
@@ -1,6 +1,18 @@
 '''
 yoodoo request
 '''
+
+# import functions
+from pyspark import SparkContext, SparkConf, HiveContext
+import pyspark.sql.functions as F
+import pyspark.sql.types as T
+from math import radians, cos, sin, asin, sqrt
+from pyspark.sql.functions import *
+from pyspark.sql.window import Window
+from pyspark.sql.functions import lit
+from pyspark.sql.types import StringType
+
+
 # ingest household id data
 
 hid_path = "s3a://ada-platform-components/Household/household/MY/restrict_ip/2020-06-01_2020-06-30/*"
@@ -8,4 +20,19 @@ hid_df  = spark.read.parquet(hid_path)
 
 # ingest telco market insight data
 
-tmi_path = 's3a://ada-geogrid-feature-set/telco_market_insight'
+tmi_path = 's3a://ada-geogrid-feature-set/telco_market_insight/high_quality/MY/2021*'
+tmi_df = spark.read.parquet(tmi_path).select('ifa','cellular_carriers')
+
+# join on ifa
+
+jdf = hid_df.join(tmi_df, on='ifa', how='inner')
+
+# explode the carriers column
+
+jdfx = jdf.withColumn('carriers',F.explode('cellular_carriers')).drop('cellular_carriers')
+
+
+# groupby and collect list
+
+jdf1 = jdfx.groupby('component').agg(F.collect_list('carriers'))
+jdf1.take(5)
